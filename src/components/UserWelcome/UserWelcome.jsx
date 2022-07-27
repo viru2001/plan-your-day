d3ccd3549861ae1d599813fd0663aa7ef0820631
viru2001 <vireshfegade@gmail.com>
2022-07-27 17:20:02 +0530

    feat : add edit name functionality

--- src/components/UserWelcome/UserWelcome.jsx
+++ src/components/UserWelcome/UserWelcome.jsx
@@ -2,12 +2,14 @@ import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Moment from "react-moment";
 import moment from "moment";
+import { Edit } from "@mui/icons-material";
 
 import { useUser } from "../../contexts";
+import { CssTextField } from "../";
 
 const UserWelcome = () => {
   const {
-    userState: { name },
+    userState: { name, showEditIcon, showNameField },
     userDispatch,
   } = useUser();
 
@@ -32,8 +34,69 @@ const UserWelcome = () => {
       </Typography>
       <Box sx={{ display: "flex", alignItems: "center" }}>
         <Typography variant="h3" component="h2" sx={{ fontWeight: "bold" }}>
-          {`${greeting} ${name}`}
+          {greeting}
         </Typography>
+        <Box
+          sx={{ display: "flex", alignItems: "center" }}
+          onMouseOver={() =>
+            userDispatch({ type: "SHOW_EDIT_ICON", payload: true })
+          }
+          onMouseOut={() =>
+            userDispatch({ type: "SHOW_EDIT_ICON", payload: false })
+          }
+        >
+          {!showNameField && (
+            <Typography
+              variant="h3"
+              component="h2"
+              sx={{ fontWeight: "bold", ml: 2 }}
+            >
+              {name}
+            </Typography>
+          )}
+
+          {showEditIcon && (
+            <Edit
+              sx={{ cursor: "pointer", fontSize: 50 }}
+              onClick={() => {
+                userDispatch({
+                  type: "SHOW_EDIT_ICON",
+                  payload: false,
+                });
+                userDispatch({
+                  type: "SHOW_NAME_FIELD",
+                  payload: true,
+                });
+              }}
+            />
+          )}
+        </Box>
+        {showNameField && (
+          <CssTextField
+            variant="standard"
+            InputProps={{
+              inputProps: {
+                style: {
+                  fontSize: 40,
+                  textAlign: "center",
+                  fontWeight: "bold",
+                },
+              },
+            }}
+            sx={{ width: 300 }}
+            value={name}
+            onChange={e =>
+              userDispatch({ type: "EDIT_USERNAME", payload: e.target.value })
+            }
+            onKeyDown={e => {
+              if (e.key === "Enter") {
+                userDispatch({ type: "SHOW_EDIT_ICON", payload: false });
+                userDispatch({ type: "SHOW_NAME_FIELD", payload: false });
+                localStorage.setItem("name", name);
+              }
+            }}
+          />
+        )}
       </Box>
     </Box>
   );
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,6 +1,8 @@
 const userInitialState = {
   name: localStorage.getItem("name") || "",
   isNameSaved: localStorage.getItem("isNameSaved") || false,
+  showEditIcon: false,
+  showNameField: false,
 };
 const userReducer = (state, { type, payload }) => {
   switch (type) {
@@ -8,6 +10,10 @@ const userReducer = (state, { type, payload }) => {
       return { ...state, name: payload };
     case "SAVE_NAME":
       return { ...state, isNameSaved: true };
+    case "SHOW_EDIT_ICON":
+      return { ...state, showEditIcon: payload };
+    case "SHOW_NAME_FIELD":
+      return { ...state, showNameField: payload };
     default:
       return state;
   }
